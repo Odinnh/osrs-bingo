ae56ece8f2b190687ade402f15ba9df3411ac01e
Odinn H <84709673+Odinnh@users.noreply.github.com>
2023-04-22 19:44:43 +0200

    bla bla

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import BoardView from '@/views/BoardView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
