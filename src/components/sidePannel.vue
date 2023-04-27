778e1fbb5a7c9c4f530297cf480b474ae8b33a6d
Odinn H <84709673+Odinnh@users.noreply.github.com>
2023-04-27 15:52:35 +0200

    added moderator views

--- src/components/sidePannel.vue
+++ src/components/sidePannel.vue
@@ -18,8 +18,9 @@ const tileData = computed(() => props.tileData)
         <BoardTile :tile="tileData" :collected="props.collected"/>
         <h2>{ {{ tileData.id.split('')[0] }} , {{ tileData.id.split('')[1]  }} }</h2>
     <h1>{{ tileData.title }}</h1>
-    <p>{{ tileData.description }}</p><button class="prevent-select">Toggle Collected</button>
+    <p>{{ tileData.description }}</p><button class="prevent-select">Mark Collected</button>
     <!-- <h2>{1, 1}</h2>
     <h1>boop-a-snoot</h1>
     <p>boop froge nose pls &lt;3 </p><button class="prevent-select">Toggle Collected</button> -->
-</div></template>
+</div>
+</template>
