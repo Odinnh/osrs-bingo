e967c9b6593ad72fef2ef1c19498bca717333017
Odinn H <84709673+Odinnh@users.noreply.github.com>
2023-05-13 22:13:37 +0200

    dynamic board size

--- src/views/newBoard.vue
+++ src/views/newBoard.vue
@@ -1,17 +1,33 @@
 <template>
   <h1>createNewBoard</h1>
-  <main class="bingo-board">
-    <template v-for="index of width * height" :key="index">
-      <emptyTile :tileID="index" :width="width" :height="height" />
+  <div>
+    width: <input min="0" max="15" name="width" type="range" :value="width" /> height:
+    <input min="0" max="15" name="height" type="range" :value="height" />
+  </div>
+  <main v-if="tiles" class="bingo-board">
+    <template v-for="tile of tiles" :key="tile.coordinates">
+      <emptyTile :tile="tile" :width="width" :height="height" />
     </template>
   </main>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import emptyTile from '../components/emptyTile.vue'
 const width = ref(1)
-const height = ref(1)
+const height = ref(6)
+const tiles = computed(() => {
+  let tempObject = {}
+  for (let i = 1; i <= width.value * height.value - 1; i++) {
+    if (width.value * height.value >= 100) {
+      prefix = 0
+    }
+    let coords = (((i - 1) % width.value) + 1) * 10 + Math.floor((i - 1) / width.value) + 1
+    tempObject[coords] = { coordinates: coords }
+    console.log(tempObject)
+  }
+  return tempObject
+})
 </script>
 
 <style scoped>
@@ -27,9 +43,5 @@ const height = ref(1)
   grid-template-rows: repeat(v-bind('height'), 1fr);
   gap: 5px;
   padding: 20px;
-  user-select: none;
-}
-.scoreCard {
-  padding: 10px;
 }
 </style>
