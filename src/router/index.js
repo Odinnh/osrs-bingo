6555df1b2f911209b4cef8e7db0dd725bedbb0e1
Odinn H <84709673+Odinnh@users.noreply.github.com>
2023-05-27 13:20:49 +0200

    reverted route change

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import BoardView from '@/views/BoardView.vue'
 import ModeratorView from '@/views/ModeratorView.vue'
 import HomeView from '@/views/HomeView.vue'
@@ -8,65 +8,63 @@ import StatsScreen from '@/views/StatsScreen.vue'
 import EditBoard from '../views/EditBoard.vue'
 import loginView from '../views/loginView.vue'
 import GroupView from '../views/GroupView.vue'
-const prePath = ''
-// const prePath = '/osrs-bingo'
 const router = createRouter({
-  history: createWebHistory(),
-  // mode: 'hash',
+  history: createWebHashHistory(),
+  mode: 'hash',
   routes: [
     {
-      path: prePath + '/',
+      path: '/',
       name: 'Home',
       component: HomeView
     },
     {
-      path: prePath + '/new',
+      path: '/new',
       name: 'newBoard',
       component: newBoard
     },
     {
-      path: prePath + '/login',
+      path: '/login',
       name: 'loginView',
       component: loginView
     },
     {
-      path: prePath + '/edit/:boardUUID',
+      path: '/edit/:boardUUID',
       name: 'editBoard',
       props: true,
       component: EditBoard
     },
     {
-      path: prePath + '/groups/:boardUUID',
+      path: '/groups/:boardUUID',
       name: 'groupView',
       props: true,
       component: GroupView
     },
     {
-      path: prePath + '/b/:boardUUID',
+      path: '/b/:boardUUID',
       name: 'overview',
       props: true,
       component: BoardView
     },
     {
-      path: prePath + '/boards/',
+      path: '/boards/',
       name: 'boardOverview',
       props: true,
       component: BoardList
     },
     // {
-    //   path: prePath + '/b/:boardUUID/team/:teamCode',
+    //   path: '/b/:boardUUID/team/:teamCode',
     //   name: 'private-board',
     //   props: true,
     //   component: BoardView
     // },
     {
-      path: prePath + '/b/:boardUUID/stats/',
+      path: '/b/:boardUUID/stats/',
       name: 'stats-graph',
       props: true,
       component: StatsScreen
     },
     {
-      path: prePath + '/m/:boardUUID',
+      path: '/m/:boardUUID',
       name: 'moderator',
       props: true,
       component: ModeratorView
