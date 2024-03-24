5b8d66144cc37ba66e6425636d7aeaf2aab9e812
Odinnh <odin.hullekes@gmail.com>
2024-03-24 22:18:38 +0100

    sortable list done

--- src/views/sortableTilesTest.vapor.vue
+++ src/views/sortableTilesTest.vapor.vue
@@ -1,48 +1,82 @@
 <template>
   <main ref="el">
-    <div class="tile" v-for="tile in sortedList">
+    <div class="tile" v-for="tile in sortedList" :key="tile.id">
       {{ tile.title }}
+      <button @click="RemoveTileFromList(list, tile.id)">X</button>
     </div>
+    <button v-if="list.length < 25" class="add tile" @click="AddTileToList(list)">+</button>
   </main>
 </template>
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { useSortable } from '@vueuse/integrations/useSortable'
-
+import { uid } from 'uid'
+import { ref, computed, nextTick } from 'vue'
+import { useSortable, moveArrayElement } from '@vueuse/integrations/useSortable'
+interface Tile {
+  id: string
+  title: string
+}
 const el = ref<HTMLElement | null>(null)
-const list = ref<object[]>([
-  { id: 1, title: 1 },
-  { id: 2, title: 2 },
-  { id: 3, title: 3 },
-  { id: 4, title: 4 },
-  { id: 5, title: 5 }
-])
-const orderOfList = ref<number[]>([2, 1, 3, 4, 5])
-const sortedList = computed<object[]>(() => {
+const list = ref<Tile[]>([])
+const orderOfList = ref<string[]>([])
+for (let i = 0; i < 25; i++) {
+  let uuid = uid(4)
+  list.value[i] = <Tile>{ id: uuid, title: uuid }
+  orderOfList.value[i] = uuid
+}
+
+const sortedList = computed<Tile[]>(() => {
   return list.value.toSorted(
     (a, b) => orderOfList.value.indexOf(a.id) - orderOfList.value.indexOf(b.id)
   )
 })
 
 list.value.sort()
-useSortable(el, orderOfList)
+useSortable(el, sortedList, {
+  inverted: true,
+  ghostClass: 'blue-background-class',
+  onUpdate: (e: any) => {
+    moveArrayElement(sortedList.value, e.oldIndex, e.newIndex)
+
+    nextTick(() => {
+      orderOfList.value = sortedList.value.map((tile) => tile.id)
+    })
+  }
+})
+const AddTileToList = (list: Tile[]) => {
+  const uuid = uid(4)
+  list.push({ id: uuid, title: uuid })
+  orderOfList.value.push(uuid)
+}
+const RemoveTileFromList = (list: Tile[], id: string) => {
+  list.splice(
+    list.findIndex((tile) => tile.id === id),
+    1
+  )
+  orderOfList.value = sortedList.value.map((tile) => tile.id)
+}
 </script>
 <style scoped>
 main {
   display: grid;
+  align-content: flex-start;
   grid-template-columns: repeat(5, 1fr);
   gap: 20px;
   width: 500px;
-  height: 500px;
+  padding: 20px;
   background-color: grey;
   & .tile {
     width: 80px;
     height: 80px;
+    aspect-ratio: 1;
     background-color: white;
     text-align: center;
     line-height: 80px;
     font-size: 20px;
     color: black;
+    &.blue-background-class {
+      background-color: #7e7d7d;
+      color: white;
+    }
   }
 }
 </style>
