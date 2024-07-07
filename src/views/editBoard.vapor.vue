2556e535ff9ed445989003b2b12dfa0d6042b11b
Odinnh <odin.hullekes@gmail.com>
2024-07-07 17:09:45 +0200

    redone metrics collection

--- src/views/editBoard.vapor.vue
+++ src/views/editBoard.vapor.vue
@@ -579,6 +579,15 @@ const gatherWomEXP = async () => {
 		boardData.value!.trackingMetrics.push('hitpoints')
 		await Promise.all(
 			boardData.value!.trackingMetrics.map(async (metric: WOMMetric) => {
+				const MetricTimestampRef = doc(
+					db,
+					'Boards',
+					route.params.boardUUID as string,
+					'Metrics',
+					metric,
+					'Timestamps',
+					currentTime.toString() // Use the current timestamp as the document ID
+				)
 				const MetricRef = doc(
 					db,
 					'Boards',
@@ -595,11 +604,15 @@ const gatherWomEXP = async () => {
 							currentTime
 						)
 						// Use parsedData to update your database
-						const timeQ = 't-' + currentTime
-						const players = parsedData[timeQ]
+						const players = parsedData['t-' + currentTime]
+						batch.set(
+							MetricTimestampRef,
+							{ players: players, metric: metric, timestamp: currentTime },
+							{ merge: true }
+						)
 						batch.set(
 							MetricRef,
-							{ [currentTime]: players, metric: metric, timestamp: currentTime },
+							{ latestUpdate: { players: players, timestamp: currentTime } },
 							{ merge: true }
 						)
 					})
